2242d00966f0b1149b49f28c7bccd94a4461f834
manikrishna2851 <61704051+manikrishna2851@users.noreply.github.com>
2020-07-10 14:30:42 -0400

    Add files via upload
    Solution

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,54 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.events = [] 
+        self.undo_events = [] 
 
     def add(self, num: int):
-        pass
-
+        self.value += num
+        self.events.append({"add":num})
+        
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.events.append({"sub":num})
 
     def undo(self):
-        pass
+        if len(self.events) == 0:
+            print('Nothing to undo')
+            return
+        event = self.events.pop()
+        for name, value in event.items():
+            if name == 'add':
+                self.value -= value
+                self.undo_events.append({"sub":value})
+            elif name == 'sub':
+                self.value += value
+                self.undo_events.append({"add":value})
 
     def redo(self):
-        pass
+        if len(self.undo_events) == 0:
+            print('Nothing to redo')
+            return
+        undo_event = self.undo_events.pop()
+        for name, value in undo_event.items():
+            if name == 'add':
+                self.value -= value
+                self.events.append({"sub":value})
+            elif name == 'sub':
+                self.value += value
+                self.events.append({"add":value})
+                
 
     def bulk_undo(self, steps: int):
-        pass
+        if len(self.events) < steps:
+            steps = len(self.events)
+            print('Overstep')
+        for _ in range(0,steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        if len(self.undo_events) < steps:
+            steps =  len(self.undo_events)
+            print('Overstep')
+        for _ in range(0,steps):
+            self.redo()
